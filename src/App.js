2ddc7de718dabd7f174fafd2d9d2c2fc201a377f
Paweł Rokosz <rokosz.pawel@gmail.com>
2017-10-12 12:54:26 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './assets/styles/css/main.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="app">
+        app
+  		</div>
+    );
+  }
+}
+
+export default App;
